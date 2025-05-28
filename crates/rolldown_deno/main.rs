fb8fade9944ba67d94e5aa31c88e1783abf1151b
David Sherret <dsherret@gmail.com>
2025-05-28 17:15:49 -0400

    working

--- crates/rolldown_deno/main.rs
+++ crates/rolldown_deno/main.rs
@@ -1,21 +1,18 @@
-use std::{rc::Rc, sync::Arc, time::SystemTime};
+use std::sync::Arc;
 
 use deno_cache_dir::file_fetcher::{
-  AuthTokens, BlobData, CacheSetting, FetchNoFollowOptions, FileFetcher, FileFetcherOptions,
-  HeaderMap, NullBlobStore, NullMemoryFiles, SendError, SendResponse,
+  CacheSetting, HeaderMap, NullBlobStore, SendError, SendResponse,
 };
-use deno_config::workspace::{WorkspaceDirectory, WorkspaceDiscoverOptions};
-use deno_graph::MediaType;
+use deno_graph::{DefaultModuleAnalyzer, MediaType, Module, ModuleGraph};
 use deno_resolver::{
-  DefaultDenoResolverRc, DenoResolver, DenoResolverOptions, NodeAndNpmReqResolver,
   factory::{ResolverFactory, WorkspaceFactory},
-  npm::{ByonmNpmResolverCreateOptions, NpmReqResolver, NpmReqResolverOptions},
-  workspace::ResolutionKind,
+  file_fetcher::{
+    DenoGraphLoader, DenoGraphLoaderOptions, PermissionedFileFetcher,
+    PermissionedFileFetcherOptions,
+  },
+  graph::DefaultDenoResolverRc,
+  workspace::ScopedJsxImportSourceConfig,
 };
-use node_resolver::{
-  ConditionsFromResolutionMode, IsBuiltInNodeModuleChecker, NodeResolver, PackageJsonResolver,
-};
-use reqwest::Response;
 use rolldown::{
   Bundler, BundlerOptions, ChunkFilenamesOutputOption, InputItem, ModuleType, SourceMapType,
 };
@@ -29,7 +26,8 @@ use url::Url;
 #[derive(Debug)]
 struct HttpImportPlugin {
   resolver: DefaultDenoResolverRc<RealSys>,
-  file_fetcher: Arc<FileFetcher<NullBlobStore, RealSys, RolldownHttpClient>>,
+  file_fetcher: Arc<PermissionedFileFetcher<NullBlobStore, RealSys, RolldownHttpClient>>,
+  graph: ModuleGraph,
 }
 
 impl Plugin for HttpImportPlugin {
@@ -49,13 +47,15 @@ impl Plugin for HttpImportPlugin {
     let resolved = self.resolver.resolve(
       args.specifier,
       &referrer,
+      deno_graph::Position::zeroed(),
       match args.kind {
         ImportKind::Require => node_resolver::ResolutionMode::Require,
         _ => node_resolver::ResolutionMode::Import,
       },
       node_resolver::NodeResolutionKind::Execution,
     )?;
-    Ok(Some(HookResolveIdOutput { id: resolved.url.to_string().into(), ..Default::default() }))
+    let resolved = self.graph.resolve(&resolved);
+    Ok(Some(HookResolveIdOutput { id: resolved.to_string().into(), ..Default::default() }))
   }
 
   #[allow(clippy::print_stdout)]
@@ -68,54 +68,61 @@ impl Plugin for HttpImportPlugin {
     let url = Url::parse(args.id)?;
     let file_fetcher = self.file_fetcher.clone();
 
-    tokio::task::spawn_blocking(|| {
-      // super inefficient...
-      let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
-      rt.block_on(async move {
-        let file_or_redirect = file_fetcher
-          .fetch_no_follow(
-            &url,
-            FetchNoFollowOptions {
-              maybe_auth: None,
-              maybe_checksum: None,
-              maybe_accept: None,
-              maybe_cache_setting: None,
-            },
-          )
-          .await?;
-        match file_or_redirect {
-          deno_cache_dir::file_fetcher::FileOrRedirect::File(file) => {
+    match self.graph.get(&url) {
+      Some(Module::Js(js)) => Ok(Some(rolldown_plugin::HookLoadOutput {
+        code: js.source.to_string(),
+        module_type: Some(media_to_module_type(js.media_type)),
+        ..Default::default()
+      })),
+      Some(Module::Json(json)) => Ok(Some(rolldown_plugin::HookLoadOutput {
+        code: json.source.to_string(),
+        module_type: Some(media_to_module_type(json.media_type)),
+        ..Default::default()
+      })),
+      Some(Module::Wasm(_wasm)) => {
+        panic!("Not supported.")
+      }
+      Some(Module::Node(_) | Module::Npm(_) | Module::External(_)) | None => {
+        tokio::task::spawn_blocking(|| {
+          // super inefficient...
+          let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
+          rt.block_on(async move {
+            let file = file_fetcher.fetch_bypass_permissions(&url).await?;
             Ok(Some(rolldown_plugin::HookLoadOutput {
               code: String::from_utf8_lossy(&file.source).to_string(),
-              module_type: Some(
-                match MediaType::from_specifier_and_headers(&url, file.maybe_headers.as_ref()) {
-                  MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => ModuleType::Js,
-                  MediaType::Jsx => ModuleType::Jsx,
-                  MediaType::TypeScript
-                  | MediaType::Mts
-                  | MediaType::Cts
-                  | MediaType::Dts
-                  | MediaType::Dmts
-                  | MediaType::Dcts => ModuleType::Ts,
-                  MediaType::Tsx => ModuleType::Tsx,
-                  MediaType::Css => ModuleType::Css,
-                  MediaType::Json => ModuleType::Json,
-                  MediaType::Wasm => ModuleType::Custom("wasm".to_string()),
-                  MediaType::SourceMap => ModuleType::Custom("map".to_string()),
-                  MediaType::Unknown => ModuleType::Asset,
-                  MediaType::Html => ModuleType::Custom("html".to_string()),
-                  MediaType::Sql => ModuleType::Asset,
-                },
-              ),
+              module_type: Some(media_to_module_type(MediaType::from_specifier_and_headers(
+                &url,
+                file.maybe_headers.as_ref(),
+              ))),
               ..Default::default()
             }))
-          }
-          deno_cache_dir::file_fetcher::FileOrRedirect::Redirect(url) => todo!(),
-        }
-      })
-    })
-    .await
-    .unwrap()
+          })
+        })
+        .await
+        .unwrap()
+      }
+    }
+  }
+}
+
+fn media_to_module_type(media_type: MediaType) -> ModuleType {
+  match media_type {
+    MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => ModuleType::Js,
+    MediaType::Jsx => ModuleType::Jsx,
+    MediaType::TypeScript
+    | MediaType::Mts
+    | MediaType::Cts
+    | MediaType::Dts
+    | MediaType::Dmts
+    | MediaType::Dcts => ModuleType::Ts,
+    MediaType::Tsx => ModuleType::Tsx,
+    MediaType::Css => ModuleType::Css,
+    MediaType::Json => ModuleType::Json,
+    MediaType::Wasm => ModuleType::Custom("wasm".to_string()),
+    MediaType::SourceMap => ModuleType::Custom("map".to_string()),
+    MediaType::Unknown => ModuleType::Asset,
+    MediaType::Html => ModuleType::Custom("html".to_string()),
+    MediaType::Sql => ModuleType::Asset,
   }
 }
 
@@ -153,7 +160,7 @@ impl deno_cache_dir::file_fetcher::HttpClient for RolldownHttpClient {
   }
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 #[allow(clippy::print_stdout)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let sys = RealSys;
@@ -161,26 +168,58 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let workspace_factory = Arc::new(WorkspaceFactory::new(sys.clone(), cwd, Default::default()));
   let resolver_factory = ResolverFactory::new(workspace_factory.clone(), Default::default());
   let resolver = resolver_factory.deno_resolver().await?;
+  let cjs_tracker = resolver_factory.cjs_tracker()?;
+  let jsx_config =
+    ScopedJsxImportSourceConfig::from_workspace_dir(workspace_factory.workspace_directory()?)?;
 
-  let file_fetcher = Arc::new(FileFetcher::new(
+  let file_fetcher = Arc::new(PermissionedFileFetcher::new(
     NullBlobStore,
-    sys.clone(),
     Arc::new(workspace_factory.http_cache()?.clone()),
     RolldownHttpClient { client: reqwest::Client::new() },
-    Arc::new(NullMemoryFiles),
-    FileFetcherOptions {
-      allow_remote: true,
-      auth_tokens: AuthTokens::new_from_sys(&sys),
-      cache_setting: CacheSetting::Use,
-    },
+    sys.clone(),
+    PermissionedFileFetcherOptions { allow_remote: true, cache_setting: CacheSetting::Use },
   ));
-  let plugin = HttpImportPlugin { file_fetcher, resolver: resolver.clone() };
+  let entrypoint = "jsr:@std/text@1";
+  let roots = Vec::from([Url::parse(entrypoint).unwrap()]);
+  let graph_resolver = resolver.as_graph_resolver(cjs_tracker, &jsx_config);
+  let module_analyzer = DefaultModuleAnalyzer::default();
+  let loader = DenoGraphLoader::new(
+    file_fetcher.clone(),
+    workspace_factory.global_http_cache()?.clone(),
+    resolver_factory.in_npm_package_checker()?.clone(),
+    workspace_factory.sys().clone(),
+    DenoGraphLoaderOptions { file_header_overrides: Default::default(), permissions: None },
+  );
+  let mut graph = deno_graph::ModuleGraph::new(deno_graph::GraphKind::CodeOnly);
+  graph
+    .build(
+      roots,
+      Vec::new(),
+      &loader,
+      deno_graph::BuildOptions {
+        is_dynamic: false,
+        skip_dynamic_deps: false,
+        module_info_cacher: Default::default(),
+        executor: Default::default(),
+        locker: None,
+        file_system: &sys,
+        jsr_url_provider: Default::default(),
+        passthrough_jsr_specifiers: false,
+        module_analyzer: &module_analyzer,
+        npm_resolver: None,
+        reporter: None,
+        resolver: Some(&graph_resolver),
+      },
+    )
+    .await;
+  graph.valid().unwrap();
 
+  let plugin = HttpImportPlugin { file_fetcher, resolver: resolver.clone(), graph };
   let mut bundler = Bundler::with_plugins(
     BundlerOptions {
       input: Some(vec![InputItem {
         name: Some("text".to_string()),
-        import: "jsr:@std/text@1".to_string(),
+        import: entrypoint.to_string(),
       }]),
       entry_filenames: Some(ChunkFilenamesOutputOption::String("[name].bundle.js".to_string())),
       cwd: Some(workspace::crate_dir("rolldown").join("./examples").normalize()),
